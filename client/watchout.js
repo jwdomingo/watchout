a9b6cc8a61e85e4e00fbacb60f6b18b2cd710c0f
jwdomingo <johnwill.domingo@gmail.com>
2016-01-22 15:33:56 -0800

    Create player class and append to SVG

--- client/watchout.js
+++ client/watchout.js
@@ -39,8 +39,19 @@ var update = function(data) {
   enemySelection.exit().remove();
 };
 
+var Player = function() {};
+
+Player.prototype.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+
 var dataset = createEnemies(15);
 update(dataset);
+
+var newPlayer = new Player();
+
+var start = (width * 0.5) + ',' + (height * 0.5);
+
+svg.append('path').attr('d', newPlayer.path)
+  .attr({'transform': 'translate(' + start + ') scale(2)'});
 
 setInterval(function() {
   dataset = createEnemies(15);
