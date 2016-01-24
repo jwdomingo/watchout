5e715b80c9e471f0cea525afe5496e4971742558
jwdomingo <johnwill.domingo@gmail.com>
2016-01-23 18:32:29 -0800

    Load json file into d3 parser

--- client/advanced/speech.js
+++ client/advanced/speech.js
@@ -1,3 +1,10 @@
 d3.json('trump.json', function(error, data) {
   var text = data.speech;
+
+  var minText;
+  var chars = "!@#$^&%*()+=-[]/'{}|:<>?,.";
+  for (var i = 0; i < chars.length; i++) {
+      minText = text.replace(new RegExp("\\" + chars[i], 'gi'), '');
+  }
+  console.log(minText);
 });
